46767642ff54a3a3cb8e9024fe4d104edb6ccf7a
N'DA Emmanuel <emmenuelnda2022@yahoo.com>
2026-06-16 16:03:09 +0200

    feat: gestion des evenements de clic sur la navigation et le bouton RDV

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('garage-spl92-front');
+export class AppComponent {
+  // Liste de ta flotte de camions
+  vehicules = [
+    { id: 1, marque: 'Freightliner', modele: 'Classic XL Chromé', statut: 'Disponible', image: 'alles-transport-4887423_1920.jpg' },
+    { id: 2, marque: 'Volvo Trucks', modele: 'FH 460 Citerne', statut: 'En Maintenance', image: 'hpgruesen-tank-truck-1388933_1920 (2).jpg' },
+    { id: 3, marque: 'MAN', modele: 'TGX Container Rouge', statut: 'Disponible', image: 'kalhh-truck-333251_1920.jpg' },
+    { id: 4, marque: 'Peterbilt', modele: '389 Confort USA', statut: 'Disponible', image: 'rauschenberger-usa-3208827_1920.jpg' }
+  ];
+
+  // --- ACTIONS DES BOUTONS DU MENU NAVIGATION ---
+  naviguerVers(page: string) {
+    alert(`Navigation vers la section : ${page}.\n(Dans une application complète, cela activerait le Router d'Angular pour changer de page)`);
+  }
+
+  // --- ACTION DU BOUTON VERT FLASHY ---
+  ouvrirFormulaireRDV() {
+    alert(`Ouverture du module de Prise de Rendez-vous du Garage SPL 92.\nUne notification de demande de réservation va être envoyée à l'atelier de Saint-Cloud.`);
+  }
+
+  // --- ACTION DES CARTES ---
+  voirDetails(marque: string, modele: string) {
+    alert(`Demande d'informations pour le véhicule : ${marque} - ${modele}.\nNotre équipe de Saint-Cloud va vous recontacter.`);
+  }
 }
